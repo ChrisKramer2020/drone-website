a7fc65d7a732b43235e1d46f07e8ebc144bb6794
Christopher Kramer <Hendrix1957@gmail.com>
2017-08-16 17:41:06 -0400

    carousel push

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import './styles/screen.scss'
 const main = () => {
   document.querySelector('h1').textContent += ''
   var dis = document.getElementById('Wel').style.display = none;
+  var images = [
+  'http://8k.hdwallpaperup.com/wp-content/uploads/2016/12/Jones-Beach-Australia-4K-Wallpaper.jpg',
+  'http://8k.hdwallpaperup.com/wp-content/uploads/2017/02/Musha-Cay-Island-in-the-Bahamas-5K-Wallpaper-680x425.jpg',
+  'http://8k.hdwallpaperup.com/wp-content/uploads/2017/01/Lake-Sunset-Trees-8K-Wallpaper-680x425.jpg',
+  'http://8k.hdwallpaperup.com/wp-content/uploads/2016/12/Jones-Beach-Australia-4K-Wallpaper.jpg',
+  'https://tse1.mm.bing.net/th?&id=OIP.M36006a548e63f173365df6e89e14aba6o0&w=310&h=173&c=0&pid=1.9&rs=0&p=0&r=0',
+  'https://tse4.mm.bing.net/th?id=OIP.tVABsB9ks01XbiMvnO7rpAFNC7&w=244&h=160&c=7&qlt=90&o=4&pid=1.7'
+];
+
+var num=0;
+var slider;
+function Next() {
+  var slider=document.getElementById('workin'); num++;
+  if (num >= images.length) {
+    num=0;
+  }
+  slider.src=images[num]
+}
+
+function Prev() {
+  var slider=document.getElementById('workin'); num--;
+  if (num < 0) {
+    num=images.length-1;
+  }
+  slider.src=images[num];
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
